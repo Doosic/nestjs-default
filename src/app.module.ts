a447d47a52eaccdb78696812cde441e874d0821e
doosicEE <strou73@naver.com>
2023-01-18 14:17:43 +0900

    #Add TypeORM 설정(postgresql사용)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BoardsModule } from './boards/boards.module';
+import { TypeOrmModule } from '@nestjs/typeorm/dist';
+import { typeROMConfig } from './configs/typeorm.config';
 
+// ORM 설정시에는 메인 모듈에 IMPORT해줘야 한다.
 @Module({
-  imports: [BoardsModule],
+  imports: [TypeOrmModule.forRoot(typeROMConfig), BoardsModule],
 })
 export class AppModule {}
